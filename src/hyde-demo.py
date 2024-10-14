461e0665772b96c3103a01e633e8265a2a17c339
Philip Offermans <u440541@byzantium.uvt.nl>
2024-10-14 14:40:24 +0200

    changed script to use ollama generator

--- src/hyde-demo.py
+++ src/hyde-demo.py
@@ -4,20 +4,20 @@ from pyserini.search.faiss import AutoQueryEncoder
 from hyde import Promptor, OpenAIGenerator, CohereGenerator, HyDE
 
 # Replace with your API key (OpenAI or Cohere)
-KEY = '<api key>'
+KEY = 'REDACTED'
 
 # Adding print statements to track progress
 print("Initializing Promptor...")
 promptor = Promptor('web search')
 
 print("Initializing OpenAIGenerator with model 'text-davinci-003'...")
-generator = OpenAIGenerator('text-davinci-003', KEY)
+generator = OpenAIGenerator('gpt-3.5-turbo', KEY)
 
 print("Initializing AutoQueryEncoder with 'facebook/contriever' model...")
 encoder = AutoQueryEncoder(encoder_dir='facebook/contriever', pooling='mean')
 
 print("Initializing FaissSearcher with 'contriever_msmarco_index/'...")
-searcher = FaissSearcher('contriever_msmarco_index/', encoder)
+searcher = FaissSearcher('./src/contriever_msmarco_index/', encoder)
 
 print("Loading LuceneSearcher prebuilt index 'msmarco-v1-passage'...")
 corpus = LuceneSearcher.from_prebuilt_index('msmarco-v1-passage')
@@ -26,3 +26,29 @@ print("Initializing HyDE with Promptor, Generator, Encoder, and Searcher...")
 hyde = HyDE(promptor, generator, encoder, searcher)
 
 print("Initialization complete. Ready to proceed!")
+
+query = 'how long does it take to remove wisdom tooth'
+
+prompt = hyde.prompt(query)
+print(prompt)
+
+hypothesis_documents = hyde.generate(query)
+for i, doc in enumerate(hypothesis_documents):
+    print(f'HyDE Generated Document: {i}')
+    print(doc.strip())
+
+hyde_vector = hyde.encode(query, hypothesis_documents)
+print(hyde_vector.shape)
+
+
+hits = hyde.search(hyde_vector, k=10)
+for i, hit in enumerate(hits):
+    print(f'HyDE Retrieved Document: {i}')
+    print(hit.docid)
+    print(json.loads(corpus.doc(hit.docid).raw())['contents'])
+
+hits = hyde.e2e_search(query, k=10)
+for i, hit in enumerate(hits):
+    print(f'HyDE Retrieved Document: {i}')
+    print(hit.docid)
+    print(json.loads(corpus.doc(hit.docid).raw())['contents'])
