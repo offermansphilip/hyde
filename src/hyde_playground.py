9b3bdc89cbf45c8de27e04d69ee3f4187c0cf226
Philip Offermans <u440541@aurometalsaurus.uvt.nl>
2024-10-18 15:41:14 +0200

    added code for HyDE with improved query

--- src/hyde_playground.py
+++ src/hyde_playground.py
@@ -24,21 +24,64 @@ generator = OllamaGenerator(model_name='llama3.1')  # Use Llama 3.1 with Ollama
 # Initialize the HyDE engine with the promptor, generator, encoder, and searcher
 hyde = HyDE(promptor=promptor, generator=generator, encoder=query_encoder, searcher=searcher)
 
+print("____IMPROVED_HYDE____")
 # Perform retrieval and evaluation with generated passages
-for qid in tqdm(topics):
-    if qid in qrels:
-        query = topics[qid]['title']
-        print(f"Query: {query}")
-        # Generate hypotheses documents using HyDE (llama3.1)
-        hypothesis_documents = hyde.generate(query)
-        print(f"Hypothesis Documents: {hypothesis_documents}")
+with open('./runs_playground/improved-hyde-dl19-contriever-llama3.1-top1000-8rep-trec', 'w') as f, open('./runs_playground/improved-hyde-dl19-llama3.1-gen.jsonl', 'w') as fgen:
+    for qid in tqdm(topics):
+        if qid in qrels:
+            query = topics[qid]['title']
 
-        # Encode the query and generated documents to form the HyDE vector
-        hyde_vector = hyde.encode(query, hypothesis_documents)
+            # Improve query
+            improved_query = hyde.improved_query(query)
 
-        # Search using the HyDE vector
-        hits = hyde.search(hyde_vector, k=1000)
-        for hit in hits:
-            print(f"Hit: {hit}")
+            # Generate hypotheses documents using HyDE (llama3.1)
+            hypothesis_documents = hyde.generate(improved_query)
+
+            # Encode the query and generated documents to form the HyDE vector
+            hyde_vector = hyde.encode(query, hypothesis_documents)
+
+            # Search using the HyDE vector
+            hits = hyde.search(hyde_vector, k=1000)
+
+            # Write results to file
+            rank = 0
+            for hit in hits:
+                rank += 1
+                f.write(f'{qid} Q0 {hit.docid} {rank} {hit.score} rank\n')
+
+# Evaluation commands
+os.system('python -m pyserini.eval.trec_eval -c -l 2 -m map dl19-passage ./runs_playground/improved-hyde-dl19-contriever-gpt3-top1000-8rep-trec')
+os.system('python -m pyserini.eval.trec_eval -c -m ndcg_cut.10 dl19-passage ./runs_playground/improved-hyde-dl19-contriever-gpt3-top1000-8rep-trec')
+os.system('python -m pyserini.eval.trec_eval -c -l 2 -m recall.1000 dl19-passage ./runs_playground/improved-hyde-dl19-contriever-gpt3-top1000-8rep-trec')
+
+# print("____NORMAL_HYDE____")
+# # Perform retrieval and evaluation with generated passages
+# with open('./runs_playground/hyde-dl19-contriever-llama3.1-top1000-8rep-trec', 'w') as f, open('./runs_playground/hyde-dl19-llama3.1-gen.jsonl', 'w') as fgen:
+#     for qid in tqdm(topics):
+#         if qid in qrels:
+#             query = topics[qid]['title']
+
+#             # Improve query
+#             improved_query = hyde.improved_query(query)
+
+#             # Generate hypotheses documents using HyDE (llama3.1)
+#             hypothesis_documents = hyde.generate(improved_query)
+
+#             # Encode the query and generated documents to form the HyDE vector
+#             hyde_vector = hyde.encode(query, hypothesis_documents)
+
+#             # Search using the HyDE vector
+#             hits = hyde.search(hyde_vector, k=1000)
+
+#             # Write results to file
+#             rank = 0
+#             for hit in hits:
+#                 rank += 1
+#                 f.write(f'{qid} Q0 {hit.docid} {rank} {hit.score} rank\n')
+
+# # Evaluation commands
+# os.system('python -m pyserini.eval.trec_eval -c -l 2 -m map dl19-passage ./runs_playground/hyde-dl19-contriever-gpt3-top1000-8rep-trec')
+# os.system('python -m pyserini.eval.trec_eval -c -m ndcg_cut.10 dl19-passage ./runs_playground/hyde-dl19-contriever-gpt3-top1000-8rep-trec')
+# os.system('python -m pyserini.eval.trec_eval -c -l 2 -m recall.1000 dl19-passage ./runs_playground/hyde-dl19-contriever-gpt3-top1000-8rep-trec')
 
 
